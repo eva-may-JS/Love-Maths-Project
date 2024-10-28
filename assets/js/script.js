8deae8ff33531060a5f89cb6e6e23850cdcdfa1c
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-10-28 18:47:24 +0000

    Add addition functionality to calculateCorrectAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 // Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
+
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
@@ -44,7 +45,23 @@ function checkAnswer () {
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer () {
+
+    //When JS gets data from the DOM it returns it as a string, so we use "parseInt" to convert it to a number
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
 
@@ -57,6 +74,7 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
+
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
